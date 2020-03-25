254966679e1080e631a3b3bdae321979519cf653
Timansy <brown.tim@gmail.com>
2020-03-24 23:55:56 -0400

    finished layout, starting on functions

--- docs/04/script.js
+++ docs/04/script.js
@@ -1,26 +1,39 @@
-var url = "https://opentdb.com/api.php?amount=10";
+// var url = "https://opentdb.com/api.php?amount=10";
 
-var request = new XMLHttpRequest()
+// var request = new XMLHttpRequest()
 
-request.open('GET','https://opentdb.com/api.php?amount=10', true);
+// request.open('GET','https://opentdb.com/api.php?amount=10', true);
 
-request.onload = function(){
-    var data = JSON.parse(this.response);
+// request.onload = function(){
+//     var data = JSON.parse(this.response);
 
-    console.log(JSON.stringify(data));
-    console.log(data);
+//     console.log(JSON.stringify(data));
+//     console.log(data);
 
-    console.log(data.results.length);
-    console.log(data.results[0].category);
-    console.log(data.results[0].type);
-    console.log(data.results[0].difficulty);
-    console.log(data.results[0].question);
-    console.log(data.results[0].correct_answer);
-    console.log(data.results[0].incorrect_answers[0]);
-    console.log(data.results[0].incorrect_answers[1]);
-    console.log(data.results[0].incorrect_answers[2]);
+//     console.log(data.results.length);
+//     console.log(data.results[0].category);
+//     console.log(data.results[0].type);
+//     console.log(data.results[0].difficulty);
+//     console.log(data.results[0].question);
+//     console.log(data.results[0].correct_answer);
+//     console.log(data.results[0].incorrect_answers[0]);
+//     console.log(data.results[0].incorrect_answers[1]);
+//     console.log(data.results[0].incorrect_answers[2]);
+// }
 
+// request.send();
 
+$(document).ready(function () {
+]
+function loadGameBoard(){
+    $("div.highscores").removeClass("active");
+    $("div.gameboard").addClass("active");
+}
+function loadHighScores(){
+    $("div.highscores").addClass("active");
+    $("div.gameboard").removeClass("active");
 }
 
-request.send();
+loadGameBoard();
+
+});
